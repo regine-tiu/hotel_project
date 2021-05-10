091c60c5d0a0cf9a251aaa2a872cb15f4003257e
regine-tiu <regine.d.tiu@gmail.com>
2021-05-10 17:36:00 +0800

    part 1 complete

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,36 @@
 class Room
+ def initialize(capacity)
+  @capacity = capacity
+  @occupants = []
+ end 
+
+ def capacity
+  @capacity
+ end
+ 
+ def occupants
+  @occupants
+ end 
+
+ def full?
+  if @occupants.length < capacity
+    false
+  else
+    true
+  end    
+ end 
+
+ def available_space
+  @capacity - @occupants.length
+ end 
+
+ def add_occupant(guest_name)
+    if self.full? == false
+      @occupants << guest_name
+      true
+    else
+      false
+    end   
+ end 
 
 end
